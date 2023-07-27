d873ebbac782b021408baeaa557957ba0d0226db
kenzalch <kenzalachgarensa@gmail.com>
2023-07-27 02:05:09 -0700

    monty

---
+++ opcode_div_muuul.c
@@ -0,0 +1,62 @@
+#include "monty.h"
+
+/**
+ * _div - divides the second element by the top element of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number;
+ * Return: no return
+ */
+void _div(stack_t **doubly, unsigned int cline)
+{
+	int l = 0;
+	stack_t *aux = NULL;
+
+	aux = *doubly;
+
+	for (; aux != NULL; aux = aux->next, l++)
+		;
+
+	if (l < 2)
+	{
+		dprintf(2, "L%u: can't div, stack too short\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*doubly)->n == 0)
+	{
+		dprintf(2, "L%u: division by zero\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	aux = (*doubly)->next;
+	aux->n /= (*doubly)->n;
+	_pop(doubly, cline);
+}
+
+/**
+ * _mul - multiplies the top element to the second top element of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number;
+ * Return: no return
+ */
+void _mul(stack_t **doubly, unsigned int cline)
+{
+	int l = 0;
+	stack_t *aux = NULL;
+
+	aux = *doubly;
+
+	for (; aux != NULL; aux = aux->next, l++)
+		;
+
+	if (l < 2)
+	{
+		dprintf(2, "L%u: can't mul, stack too short\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+}
